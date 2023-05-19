47802fd242c4b9c51edaae0176d3e4a8299e7e56
finroy <gt9322@gmail.com>
2023-05-18 21:18:59 -0400

    Change /release to use releases instead of Actions

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -5,89 +5,45 @@ import { Octokit } from "@octokit/rest";
 export const command = {
     data: new SlashCommandBuilder()
         .setName("release")
-        .setDescription("The latest release from the specified branch")
-        .addStringOption(option =>
-            option
-                .setName("branch")
-                .setDescription("Branch to retrieve latest release from")),
+        .setDescription("Get the latest release from the Strato repository"),
     level: AccessLevel.User,
     async execute(interaction: ChatInputCommandInteraction) {
         const octokit = new Octokit();
 
-        const branch = interaction.options.getString("branch") ?? "master";
-        const runs = (await octokit.actions.listWorkflowRunsForRepo({
-            owner: "strato-emu",
-            repo: "strato",
-            branch: branch,
-            status: "completed",
-        })).data;
+        try{
+            let release = await octokit.request("GET /repos/{owner}/{repo}/releases/latest", {
+                owner: "strato-emu",
+                repo: "strato",
+            });
 
-        if (runs.total_count == 0)
-            return interaction.reply({ content: `No GitHub Actions workflow found in '${branch}' branch`, ephemeral: true});
-
-        let selectedRun = null;
-        let releaseArtifact = null;
-        let debugArtifact = null;
-        for (let run of runs.workflow_runs) {
-            if (run.name == "CI") {
-                const artifacts = (await octokit.rest.actions.listWorkflowRunArtifacts({
-                    owner: "strato-emu",
-                    repo: "strato",
-                    run_id: run.id,
-                })).data.artifacts;
-
-                for (let artifact of artifacts) {
-                    if (artifact.name.endsWith("release.apk")) {
-                        releaseArtifact = artifact;
-                        if (debugArtifact)
-                            break;
-                    } else if (artifact.name.endsWith("debug.apk")) {
-                        debugArtifact = artifact;
-                        if (releaseArtifact)
-                            break;
-                    }
-                }
-
-                if (releaseArtifact != null && debugArtifact != null) {
-                    selectedRun = run;
-                    break;
-                } else {
-                    releaseArtifact = null;
-                    debugArtifact = null;
-                }
-            }
+            if (release.data.assets[0] == undefined)
+                return interaction.reply({ content: "No build attached to this release", ephemeral: true });
+    
+            let embed = new EmbedBuilder({
+                "title": release.data.name ? release.data.name : release.data.tag_name,
+                "description": release.data.body!,
+                "url": release.data.html_url,
+                "footer": {
+                    text: release.data.tag_name,
+                    icon_url: "https://raw.githubusercontent.com/strato-emu/branding/master/logo/strato-logo.png",
+                },
+                timestamp: Date.parse(release.data.created_at),
+                "author": {
+                    // We use this as a psuedo-heading rather than as an actual authorship field
+                    "name": "Latest Strato release build",
+                    "icon_url": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+                },
+                "fields": [{
+                    "name": "Release Build",
+                    "value": `[Download APK](${release.data.assets[0].browser_download_url})`
+                }]
+            });
+    
+            await interaction.reply({ embeds: [embed] });
+        } catch (err) {
+            return interaction.reply({ content: "No releases found on the repository", ephemeral: true });
         }
 
-        if (selectedRun == null || debugArtifact == null || releaseArtifact == null)
-            return interaction.reply({ content: `No CI GitHub Actions workflow found in '${branch}' branch`, ephemeral: true});
-		
-        const commit = selectedRun.head_commit!;
-        let commitTitleIndex = commit.message.indexOf("\n");
-        let embed = new EmbedBuilder({
-            "title": commitTitleIndex == -1 ? commit.message : commit.message.substring(0, commitTitleIndex),
-            "description": commitTitleIndex != -1 ? commit.message.substring(commitTitleIndex + 1) : undefined,
-            "url": selectedRun.html_url,
-            timestamp: Date.parse(selectedRun.run_started_at!),
-            "footer": {
-                "text": "A GitHub account is required to download APKs"
-            },
-            "author": {
-                // We use this as a psuedo-heading rather than as an actual authorship field
-                "name": `GitHub Actions Build on ${branch}`,
-                "icon_url": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-            },
-            "fields": [
-                {
-                    "name": "Release Build (Recommended)",
-                    "value": `[Download APK](https://github.com/strato-emu/strato/suites/${selectedRun.check_suite_id}/artifacts/${releaseArtifact.id}) — Heavily optimized with limited validation`
-                },
-                {
-                    "name": "Debug Build",
-                    "value": `[Download APK](https://github.com/strato-emu/strato/suites/${selectedRun.check_suite_id}/artifacts/${debugArtifact.id}) — Extra validation and no optimizations for debugging`
-                }
-            ]
-        });
-
-        await interaction.reply({ embeds: [embed] });
+        
     }
 };
