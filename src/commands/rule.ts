952963852bd7117d506517f39b1a486fc9875290
FinRoy <79334740+finr0y@users.noreply.github.com>
2023-08-22 11:58:42 -0400

    Update rule.ts

--- src/commands/rule.ts
+++ src/commands/rule.ts
@@ -16,35 +16,40 @@ export const command = {
     	let embed = new EmbedBuilder().setColor("Red");
         switch (ruleId) {
             case 1:
-                embed.setTitle("Rule #1 - Piracy");
-                embed.setDescription("Asking, linking or helping someone download ROMs or other copyrighted content in any way is **NOT** allowed. **Piracy is completely prohibited**.");
-                embed.addFields(
-                    { name: "Why?", value: "The sharing of [Copyrighted material](https://simple.wikipedia.org/wiki/Copyright) without a license to do so is illegal under the [Berne Convention](https://en.wikipedia.org/wiki/Berne_Convention).", inline: false},
-                    { name: "How does this harm Strato?", value: "We intend to be a completely legal emulator which have been proven to be legal by landmark cases such as [Sony Computer Entertainment, Inc. v. Connectix Corp.](https://en.wikipedia.org/wiki/Sony_Computer_Entertainment,_Inc._v._Connectix_Corp.) and [Sony Computer Entertainment America, Inc. v. Bleem LLC](https://itlaw.wikia.org/wiki/Sony_Computer_Entertainment_America_v._Bleem). Supporting users who pirate results in the legality of the emulator to come into question and will likely result in a takedown by the copyright holders.", inline: false}
-                );
+                embed.setTitle("1️⃣ Piracy is completely prohibited");
+                embed.setDescription(`Asking, linking or helping someone download ROMs or other copyrighted content in any way is NOT allowed. Failing to comply will result in losing access to emulation-related channels.`);
                 break;
 
             case 2:
-                embed.setTitle("Rule #2 - Respect");
+                embed.setTitle("2️⃣ Be respectful toward other people");
                 embed.setDescription(`Be respectful toward other people. It's fine to disagree, it's not fine to insult or attack other people.
                 * You may disagree with anyone or anything you like, but you should try to keep it to opinions, and not people.
                 * The use of derogatory slurs (Racist, sexist, homophobic, transphobic, etc) is not allowed.`);
                 break;
 
             case 3:
-                embed.setTitle("Rule #3 - Spamming");
-                embed.setDescription(`Spamming is not allowed. If you do have rather lengthy messages you want to send, and you feel like it might be considered spam, please do so in <#1109121227286659133>.
-                * In addition, using a service such as [Pastebin](https://pastebin.com/) or [Hatebin](https://hatebin.com/) is recommended for code-snippets and such.`);
+                embed.setTitle("3️⃣ Spamming is not allowed");
+                embed.setDescription(`If you do have rather lengthy messages you want to send, and you feel like it might be considered spam, please do so in <#1109121227286659133>.`);
                 break;
 
             case 4:
-                embed.setTitle("Rule #4 - Offtopic");
-                embed.setDescription("Please keep offtopic conversations in <#1104386571664371842>.");
+                embed.setTitle("4️⃣ Please keep offtopic conversations in ⁠<#1104386571664371842>.");
                 break;
 
+            case 5:
+                embed.setTitle("5️⃣ Please keep the discussion in English");
+                embed.setDescription(`This ensures a common medium to communicate and eases understanding for other server members.`);
+                break;
+                
+            case 6:
+                embed.setTitle("6️⃣ Do not distribute or discuss custom builds");
+                embed.setDescription(`If you made changes to the emulator that you want to showcase, you can do so in ⁠<#1105607582560829511> or <#⁠1104386537975713832>, but clearly label the post as a custom build.`);
+                break;
+                
             default:
                 return interaction.reply({ content: "The specified rule wasn't recognized", ephemeral: true });
         }
+        embed.setFooter("Multiple violations of the above rules may lead to temporary and eventually permanent mutes.");
     	interaction.reply({embeds: [embed]});
     }
 };
